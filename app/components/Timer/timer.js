a94b0159d991539dfbecefb4b74b4f7be2e75840
Tarık Tunç <tarktunc@hotmail.com>
2023-09-26 18:07:51 +0300

    Part of the timer working issue has been resolved.

--- app/components/Timer/timer.js
+++ app/components/Timer/timer.js
@@ -1,48 +1,100 @@
 "use client";
-
-import React, { useState, useEffect } from "react";
-import TimerCss from "./timer.module.scss";
-
-//! Redux Tool Import
+import React, { useState, useEffect, useRef } from "react";
+import TimerStyles from "./timer.module.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { resetPomoCount, incPomoCount } from "@/Redux/Slices/timerSlice";
 
+const START_MINUTES = "0";
+const START_SECOND = "3";
+const START_DURATION = 10;
+
 export default function TimerMain() {
-  //! Dispatch
   const dispatch = useDispatch();
-  //! Selector
-  const { taskName, pomoCounter, pomoTime } = useSelector(
-    (state) => state.timerSetting
-  );
-  //! useState`s
-  const [minutes, setMinutes] = useState(pomoTime);
-  const [seconds, setSeconds] = useState(0);
+  const { taskName, pomoCounter, pomoTime, shortBreak, longBreak } =
+    useSelector((state) => state.timerSetting);
 
-  // ------------------ TIMER START ------------
-  const handleStart = () => {
-    const totalSeconds = minutes * 60;
-    let remainingSeconds = totalSeconds;
-    startTimer(remainingSeconds);
+  const [currentMinutes, setMinutes] = useState(START_MINUTES);
+  const [currentSeconds, setSeconds] = useState(START_SECOND);
+  const [isStop, setIsStop] = useState(false);
+  const [duration, setDuration] = useState(START_DURATION);
+  const [isRunning, setIsRunning] = useState(false);
+
+  //------------------ TIMER COUNTER ------------
+
+  const pomodoroBtn = () => {
+    resetHandler();
+    setMinutes(pomoTime);
+    document.body.style.backgroundColor = "";
+    document.body.style.transition = "0.5s";
   };
 
-  // ------------------ TIMER COUNTER ------------
-  const startTimer = (remainingSeconds) => {
-    const interval = setInterval(() => {
-      if (remainingSeconds <= 0) {
-        clearInterval(interval);
-      } else {
-        const newMinutes = Math.floor(remainingSeconds / 60);
-        const newSeconds = remainingSeconds % 60;
-        setMinutes(newMinutes);
-        setSeconds(newSeconds);
-        remainingSeconds -= 1;
-        console.log(remainingSeconds);
-      }
-    }, 1000);
+  const shortBreakBtn = () => {
+    resetHandler();
+    setMinutes(shortBreak);
+    document.body.style.backgroundColor = "#38858A";
+    document.body.style.transition = "0.5s";
   };
+
+  const longBreakBtn = () => {
+    resetHandler();
+    setMinutes(longBreak);
+    document.body.style.backgroundColor = "#608CAB";
+    document.body.style.transition = "0.5s";
+  };
+
+  const startHandler = () => {
+    setDuration(parseInt(START_SECOND, 20) + 60 * parseInt(START_MINUTES, 10));
+    // setMinutes(60 * 5);
+    // setSeconds(0);
+    setIsRunning(true);
+  };
+
+  const stopHandler = () => {
+    // stop timer
+    setIsStop(true);
+    setIsRunning(false);
+  };
+  const resetHandler = () => {
+    setMinutes(START_MINUTES);
+    setSeconds(START_SECOND);
+    setIsRunning(false);
+    setIsStop(false);
+    setDuration(START_DURATION);
+  };
+
+  const resumeHandler = () => {
+    let newDuration =
+      parseInt(currentMinutes, 10) * 60 + parseInt(currentSeconds, 10);
+    setDuration(newDuration);
+
+    setIsRunning(true);
+    setIsStop(false);
+  };
+
+  useEffect(() => {
+    if (isRunning === true) {
+      let timer = duration;
+      var minutes, seconds;
+      const interval = setInterval(function () {
+        if (--timer <= 0) {
+          resetHandler();
+        } else {
+          minutes = parseInt(timer / 60, 10);
+          seconds = parseInt(timer % 60, 10);
+
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
+
+          setMinutes(minutes);
+          setSeconds(seconds);
+        }
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [duration, isRunning]);
 
   // ------------------ POMODO COUNTER RESET  ------------
-  function resPomoCounter() {
+  function resetPomodoroCounter() {
     let text = "Tasks Restart ?";
     // confirm() alert method.
     if (confirm(text) === true) {
@@ -52,28 +104,57 @@ export default function TimerMain() {
       alert("TASKS CANCEL");
     }
   }
+  // ------------------ POMODO STATUS BUTTON  ------------
 
   return (
-    <div className={TimerCss.container}>
-      <div className={TimerCss.timer}>
-        {/*------------------ POMO SHORT LONG BUTTON ------------*/}
-        <div className={TimerCss.button}>
-          <button className="buttonActive">Pomodoro</button>
-          <button>Short Break</button>
-          <button>Long Break</button>
+    <div className={TimerStyles.container}>
+      <div className={TimerStyles.timer}>
+        {/*------------------ POMODORO STATUS BUTTON ------------*/}
+        <div className={TimerStyles.button}>
+          <button onClick={pomodoroBtn}>Pomodoro</button>
+          <button onClick={shortBreakBtn}>Short</button>
+          <button onClick={longBreakBtn}>Long</button>
         </div>
 
         {/*------------------ TIMER ------------*/}
-        <div className={TimerCss.time}>
-          {String(minutes).padStart(2, "0")}:{String(seconds).padStart(2, "0")}
+        <div className={TimerStyles.time}>
+          {String(currentMinutes).padStart(2, "0")}:
+          {String(currentSeconds).padStart(2, "0")}
         </div>
 
         {/*------------------ START PAUSE RESET BUTTON ------------*/}
-        <div className={TimerCss.start}>
-          <div className={TimerCss.startResetButton}>
-            <button className={TimerCss.startButton}>Start</button>
-            <button className={TimerCss.resetButton}>
-              <i className="fa-solid fa-forward-step" />
+        <div className={TimerStyles.start}>
+          <div className={TimerStyles.startResetButton}>
+            {!isRunning && !isStop && (
+              <button
+                className={TimerStyles.startButton}
+                onClick={startHandler}>
+                START
+              </button>
+            )}
+            {isRunning && (
+              <button className={TimerStyles.startButton} onClick={stopHandler}>
+                PAUSE
+              </button>
+            )}
+
+            {isStop && (
+              <button
+                className={TimerStyles.startButton}
+                onClick={resumeHandler}>
+                START
+              </button>
+            )}
+            {/* >| NEXT Button */}
+            <button
+              onClick={resetHandler}
+              name="nextBtn"
+              className={TimerStyles.resetButton}>
+              <i
+                // onClick={(e) => e.target.parentNode.click()}
+                onClick={resetHandler}
+                className="fa-solid fa-forward-step"
+              />
             </button>
           </div>
         </div>
@@ -81,13 +162,13 @@ export default function TimerMain() {
 
       {/*------------------ POMODO COUNTER  ------------*/}
 
-      <button className={TimerCss.level} onClick={resPomoCounter}>
+      <button className={TimerStyles.level} onClick={resetPomodoroCounter}>
         {pomoCounter}
       </button>
 
       {/*------------------ TASK NAME ------------*/}
 
-      <div className={TimerCss.tasksLevel}>{taskName}</div>
+      <div className={TimerStyles.tasksLevel}>{taskName}</div>
     </div>
   );
 }
